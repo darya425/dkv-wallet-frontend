7f2d73c27ec2ec9d724716d72b62d00afc422281
irob4ik <theroberto@inbox.lv>
2021-11-14 22:02:29 +0200

    added navigation routes

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,46 +4,56 @@ import QueryStatsIcon from '@mui/icons-material/QueryStats';
 import EuroIcon from '@mui/icons-material/Euro';
 
 import styles from './Navigation.module.scss';
+import muiTheme from '../UI/muiTheme';
 
-const iconSize = 27;
+const iconSize = {
+    mobile: 27,
+    higher: 12,
+};
+
+const sxValues = {
+    fontSize: iconSize.mobile,
+    [muiTheme.breakpoints.up('tablet')]: {
+        fontSize: iconSize.higher,
+    },
+}
+
 
 export default function Navigation() {
     return (
-        <div className={styles.navContainer}>
-            <nav >
-                <NavLink
-                    to="/"
-                    exact={true}
-                    className={styles.link}
-                    activeClassName={styles.activeLink}
-                >
-                    <HomeIcon
-                        sx={{ fontSize: iconSize }}
-                    />
-                </NavLink>
+        <nav className={styles.navContainer}>
+            <NavLink
+                to="/"
+                exact={true}
+                className={styles.link}
+                activeClassName={styles.activeLink}
+            >
+                <HomeIcon
+                    sx={sxValues}
+                />                    
+            </NavLink>
 
-                <NavLink                    
-                    to="/statistics"
-                    exact={true}
-                    className={styles.link}
-                    activeClassName={styles.activeLink}
-                >
-                    <QueryStatsIcon
-                        sx={{ fontSize: iconSize }}
-                    />
-                </NavLink>
+            <NavLink                    
+                to="/statistics"
+                exact={true}
+                className={styles.link}
+                activeClassName={styles.activeLink}
+            >
+                <QueryStatsIcon
+                    sx={sxValues}
+                />
+            </NavLink>
 
-                <NavLink                
-                    to="/currency"
-                    exact={true}
-                    className={styles.link}
-                    activeClassName={styles.activeLink}
-                >
-                    <EuroIcon
-                        sx={{ fontSize: iconSize }}
-                    />
-                </NavLink>
-            </nav>
-        </div>        
+            <NavLink                
+                to="/currency"
+                exact={true}
+                className={styles.link+' '+styles.hidden}
+                activeClassName={styles.activeLink}
+            >
+                <EuroIcon
+                    sx={sxValues}
+                />
+            </NavLink>
+        </nav>      
     );    
 }
