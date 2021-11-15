aa05cf9ee6ad6506356da8beb5ae30fcedaf3698
Niko Yea <nikoyea14@gmail.com>
2021-11-15 16:33:53 +0100

    add request methods

--- src/Components/UI/muiTheme/muiTheme.js
+++ src/Components/UI/muiTheme/muiTheme.js
@@ -1,36 +1,38 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
 const muiTheme = createTheme({
-    palette: {
-        main: {
-            blue: "#4A56E2",        // main BLUE
-            green: "#24CCA7",       // main GREEN
-            red: "#FF6596",         // main RED
-            white: "#FFFFFF",       // WHITE
-            black: "#000000",       // BLACK 
-            background: "#E7EAF2",  // background GREY
-        },
-        neutral: {
-            grey4: "#BDBDBD",
-            grey5: "#E0E0E0",
-            pink: "#FFD8D0",
-            purple: "#C5BAFF",
-        },
+  palette: {
+    primary: {
+      main: '#24CCA7', //main GREEN
     },
-    breakpoints: {
-        values: {
-        mobile: 320,
-        tablet: 768,
-        desktop: 1280,
-        },
+    secondary: {
+      main: '#4A56E2', //main BLUE
     },
-    typography: {
-        fontFamily: [
-        'Poppins',
-        'circe',
-        'sans-serif',
-        ].join(','),        
+    // main: {
+    //   blue: '#4A56E2', // main BLUE
+    //   green: '#24CCA7', // main GREEN
+    //   red: '#FF6596', // main RED
+    //   white: '#FFFFFF', // WHITE
+    //   black: '#000000', // BLACK
+    //   background: '#E7EAF2', // background GREY
+    // },
+    neutral: {
+      grey4: '#BDBDBD',
+      grey5: '#E0E0E0',
+      pink: '#FFD8D0',
+      purple: '#C5BAFF',
     },
+  },
+  breakpoints: {
+    values: {
+      mobile: 320,
+      tablet: 768,
+      desktop: 1280,
+    },
+  },
+  typography: {
+    fontFamily: ['Poppins', 'circe', 'sans-serif'].join(','),
+  },
 });
 
 export default muiTheme;
