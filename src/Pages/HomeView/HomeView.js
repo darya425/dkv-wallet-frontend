1654c6e7ebc110cc9b008f093034046844fee428
irob4ik <theroberto@inbox.lv>
2021-11-15 22:47:45 +0200

    views and screens

--- src/Pages/HomeView/HomeView.js
+++ src/Pages/HomeView/HomeView.js
@@ -1,21 +1,12 @@
-import Navigation from '../../Components/Navigation';
-// import Currency from '../../Components/Currency';
-import Balance from '../../Components/Balance';
+import InfoBlock from '../../Components/InfoBlock';
 // import UserData from '../../Components/UserData';
-
-import styles from '../../Styles/main.scss';
 
 export default function HomeView() {
   return (
-    <>      
-      <div className={ styles.info }>
-        <div className={ styles.main }>
-            <Navigation /> 
-            <Balance />
-        </div>
-          {/* <Currency /> */}
-      </div>
-        {/* <UserData /> */}
+    <>
+      <InfoBlock />
+      
+      {/* <UserData /> */}
     </>
   );
 }
