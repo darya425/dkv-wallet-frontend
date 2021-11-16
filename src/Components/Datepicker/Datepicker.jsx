c960cc36bb1705c902739a97fce9f117561e46dd
courios-minion <okarskaolya@gmail.com>
2021-11-16 20:02:28 +0100

    added date picker

---
+++ src/Components/Datepicker/Datepicker.jsx
@@ -0,0 +1,47 @@
+import {useState, useEffect} from "react";
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+// import getDaysInMonth from 'date-fns/getDaysInMonth';
+
+const Datepicker = () => {
+  const [value, setValue] = useState(new Date());
+
+//   useEffect(() => {
+//     getCurrentMonthTransactions(initialValue);
+//     // abort request on unmount
+//     return () => ;
+//   }, []);
+
+// const handleMonthChange = (date) => {
+//     if () {
+//       // make sure that you are aborting useless requests
+//       // because it is possible to switch between months pretty quickly
+//      
+//     }
+
+//     setIsLoading(true);
+//     setTransactions([]);
+//     getCurrentMonthTransactions(date);
+//   };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        views={["year", "month"]}
+        label="Year and Month"
+        minDate={new Date("2015-01-01")}
+        maxDate={new Date("2022-06-01")}
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+		// onMonthChange={handleMonthChange}
+        renderInput={(params) => <TextField {...params} helperText={null} />}
+      />
+    </LocalizationProvider>
+  );
+}
+
+export default Datepicker;
